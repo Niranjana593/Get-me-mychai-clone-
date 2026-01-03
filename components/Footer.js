9beafd228f067287f0a9963571d05d522e98d720
Niranjan <Niranjan1@gmail.com>
2026-01-03 16:30:43 +0530

    Footer component changes successfully

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const footer = () => {
   return (
     <div className='bg-gray-600 flex justify-center  items-center  text-white h-16'>
-        <p> All the &copy; Rights are reserved </p>
+        <p> All the &copy; Rights are  </p>
     </div>
   )
 }
